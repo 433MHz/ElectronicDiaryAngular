840ec491b4851daa261d18a1313e98eeed5ee40d
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-05-14 17:19:58 +0200

    Top bar component ready

--- electronic-diary/src/app/app.module.ts
+++ electronic-diary/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { SecondTopBarComponent } from './second-top-bar/second-top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    SecondTopBarComponent
   ],
   imports: [
     BrowserModule
---
+++ electronic-diary/src/app/second-top-bar/second-top-bar.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SecondTopBarComponent } from './second-top-bar.component';
+
+describe('SecondTopBarComponent', () => {
+  let component: SecondTopBarComponent;
+  let fixture: ComponentFixture<SecondTopBarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SecondTopBarComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SecondTopBarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
